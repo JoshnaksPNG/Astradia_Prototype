03a22f037efbf37345eedd9cb9213ef590314dc3
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2024-05-13 16:45:06 -0700

    Added Basic Test NPC

---
+++ NPCScript.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class NPCScript : Node2D
+{
+	[Export]
+	Area2D InteractionArea;
+	
+	[Export]
+	Node2D PartyHead;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if(InteractionArea != null)
+		{
+			if(InteractionArea.OverlapsBody(PartyHead))
+			{
+				GD.Print("Interaction Area Reached");
+			}
+		}
+	}
+}
