dade9ab5e7183ceef5f1f3ff1b6e43e566027fd3
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2024-05-13 18:32:28 -0700

    Added NPC Interaction
    Added NPC Interaction Function
    
    Added JSON field to NPC Script for behavior

--- NPCScript.cs
+++ NPCScript.cs
@@ -1,5 +1,7 @@
 using Godot;
+using Godot.Collections;
 using System;
+using System.Diagnostics;
 
 public partial class NPCScript : Node2D
 {
@@ -10,12 +12,15 @@ public partial class NPCScript : Node2D
 	Node2D PartyHead;
 	
 	[Export]
-	Godot.File Behavior;
+	Json Behavior;
+	Dictionary<string, string> BehaviorDictionary;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	}
+		BehaviorDictionary = Behavior.Data.AsGodotDictionary<string, string>();
+
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
@@ -24,8 +29,16 @@ public partial class NPCScript : Node2D
 		{
 			if(InteractionArea.OverlapsBody(PartyHead))
 			{
-				GD.Print("Interaction Area Reached");
+				if (Input.IsActionJustPressed("ui_interact"))
+				{
+					npc_interact();
+				}
 			}
 		}
 	}
+
+	public void npc_interact()
+	{
+        Debug.WriteLine(BehaviorDictionary["on_interact"]);
+    }
 }
